bc4d855a9a59d281ebbe0875724307846a3df1af
Gabriel Candido <126776544+Cervel1n@users.noreply.github.com>
2023-04-06 08:16:33 -0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,30 @@
+package nikolas354;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.MIN_VALUE;
+        while (true) {
+            System.out.println("Digite um número:");
+            while (true) { 
+                try {
+                    num = Integer.parseInt(sc.nextLine());
+                    System.out.println("O número digitado é: "+ num);
+                    break; 
+                } catch (NumberFormatException e) {
+           
+                    System.out.println("Digite um número válido");
+                }
+            }
+        
+            if (num != Integer.MIN_VALUE){
+                break;
+
+            }
+        
+           
+        }
+    }
+}
